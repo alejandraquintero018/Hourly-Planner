7ce87cb6e724277eea66c78c3ae308e7651fa837
Alejandra Quintero <alejandraquintero@Allies-MacBook-Pro.local>
2022-08-06 16:44:45 -0700

    worked on local storage and tried to debug the color changing aspect of the code

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 
-var dayEl = $('#currentDay');
-var saveBtn = $('#save');
-var timeblockEL = $('timeblock'); 
+var saveBtn = $('#saveBtn');
+var descriptionEL = $('#description'); 
 
-//This gives me the day at the top of my Calendar 
-var day = function () {
-dayEl.text(dayjs().format('dddd, MMMM D'));
-};
-//calling my function 
-day();
+$(document).ready(function () {
 
-$('.saveBtn').on('click', function(event){
-    console.log($(this).parent());
-    console.log($(this).siblings('textarea').val()); 
-}); 
+    //This gives me the day at the top of my Calendar 
+    var day = function () {
+        $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+    };
+    //calling my function 
+    day();
+
+    //where I have my save button save things into local storage
+    $('.saveBtn').on('click', function (event) {
+        //console.log($(this).parent());
+        let plan = ($(this).siblings('textarea').val());
+        let toString = JSON.stringify(plan);
+        localStorage.setItem('Planned', toString); 
+        //let getPlan = localStorage.getItem('Planned'); 
+        //$(this).siblings('textarea').append
+
+    });
+
+    //function to compare value of the time and the value of the current hour 
+
+    var timecheck = function () {
+        $('.time-block').each(function () {
+            var blockHour = $(this).children('.hour').text();
+            var converted = JSON.stringify(blockHour);
+            let currentHour = dayjs().format('ha').toString();
+            if (converted === '3pm') {   //this comparison is where the bug is
+                $(this).children('description').addClass('present');
+            } if (currentHour != '2pm') {
+                $(this).children('.description').addClass('future');
+            }
+            else {
+
+            }
+            console.log(currentHour);
+            console.log(blockHour);
+        });
+    }
+    //calling my function
+    timecheck();
+});
+
+
+//setInterval()
+
+//local storage of the text area 
+
+
